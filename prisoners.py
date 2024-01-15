7b1a6caad04398c09cc6ba9fe1fa4512c06e61a0
poundocean <ji2037070@gmail.com>
2024-01-15 18:48:43 +0900

    feat: Create prisoner and Reset
    Creat prisoner list and Reset

--- prisoners.py
+++ prisoners.py
@@ -1,1 +1,10 @@
-print('hello')
+import random
+
+def simulate_prisoners_problem(num_prisoners):
+    # 죄수들을 초기화합니다.
+    prisoners = list(range(num_prisoners))
+    random.shuffle(prisoners)
+
+    # 자신의 번호를 찾은 죄수의 수를 초기화합니다.
+    count = 0
+
